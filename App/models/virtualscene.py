80af5c5a742c077bd01e3bb70a2b9693dcf5fe79
Vishala868 <122883377+Vishala868@users.noreply.github.com>
2025-04-16 15:32:00 -0400

    Update scenebuilder

--- App/models/virtualscene.py
+++ App/models/virtualscene.py
@@ -1,37 +1,43 @@
 from App.database import db
 
+
 class VirtualScene(db.Model):
     __tablename__ = "virtualscenes"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    panorama_url = db.Column(db.String(200), nullable=True)
-    navigation_items = db.relationship(
+    panorama_url = db.Column(db.String(200), nullable=True, unique=True)
+
+    outgoing_navigation = db.relationship(
         "NavigationItem",
-        backref=db.backref("virtualscene", lazy=True),
-        lazy=True,
-        cascade="all, delete-orphan"
-    )
-    meta_items = db.relationship(
-        "MetaItem",
-        backref=db.backref("virtualscene", lazy=True),
-        lazy=True,
+        foreign_keys="[NavigationItem.source_scene_id]",
+        back_populates="source_scene_rel",
         cascade="all, delete-orphan"
     )
 
-    def __init__(self, name, panorama_url=None, navigation_items=None, meta_items=None):
-        self.name = name
+    incoming_navigation = db.relationship(
+        "NavigationItem",
+        foreign_keys="[NavigationItem.target_scene_id]",
+        back_populates="target_scene_rel"
+    )
+
+    meta_items = db.relationship(
+        "MetaItem",
+        backref="virtualscene",
+        cascade="all, delete-orphan"
+    )
+
+    def __init__(self, panorama_url=None):
         self.panorama_url = panorama_url
-        self.navigation_items = navigation_items if navigation_items else []
-        self.meta_items = meta_items if meta_items else []
 
     def get_json(self):
         return {
             "id": self.id,
-            "name": self.name,
             "panorama_url": self.panorama_url,
-            "navigation_items": [item.get_json() for item in self.navigation_items],
-            "meta_items": [meta.get_json() for meta in self.meta_items]
+            "arrows": [item.get_json() for item in self.outgoing_navigation],
+            "highlights": [meta.get_json() for meta in self.meta_items]
         }
-
-    def __repr__(self):
-        return f"<VirtualScene {self.id}, Name: {self.name}>"
+    @classmethod
+    def get_by_id(cls, scene_id):
+        return cls.query.get(scene_id)
+    @classmethod
+    def get_by_panorama_url(cls, panorama_url):
+        return cls.query.filter_by(panorama_url=panorama_url).first()
